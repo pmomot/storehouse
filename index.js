351d7bbc0631c025ac70841f2bf5452a64000db4
Peter Momot <pmomot@corevalue.net>
2016-05-23 15:05:58 +0300

    minor updates [TG-8]

--- index.js
+++ index.js
@@ -37,25 +37,3 @@ app.use('/api', api);
 app.listen(env.NODE_PORT || config.port, env.NODE_IP || '0.0.0.0', function () {
     console.log('Application worker ' + process.pid + ' started...'); // eslint-disable-line
 });
-//var AmountType = sequelize.define('amountTypes', {
-//    value: {
-//        type: Sequelize.STRING
-//    },
-//    uaName: {
-//        type: Sequelize.STRING
-//    },
-//    engName: {
-//        type: Sequelize.STRING
-//    }
-//}, {
-//    freezeTableName: true
-//});
-//
-//
-
-
-//AmountType
-//    .sync({force: true})
-//    .then(function () {
-//        return Product.sync({force: true})
-//    });
