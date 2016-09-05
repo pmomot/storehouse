bdc042a67bf42349240d4fdff524ece68ed4c8b6
Bogdan <bpazich@admixer.ua>
2016-09-05 10:34:39 +0300

    first commit to subdomain branch add vhost plugin separate direct and subdomain request

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ var express = require('express'),
     config = require('./server/config'),
     app = express(),
     tables = require('./server/tables')(config),
-    api = require('./server/routes/api')(app, express, tables),
-    env = process.env;
+    api = require('./server/routes/api')/*(app, express, tables)*/,
+    env = process.env,
+    vhost = require("vhost");
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -22,11 +24,23 @@ app.get('/health', function (req, res) {
     res.end();
 });
 
+var direct = api.direct(app,express,tables);
+var sub = api.sub(app,express,tables);
+
+function createVirtualHost(domainName, method) {
+    return vhost(domainName, method)
+};
+
+var subhost = createVirtualHost("*.localhost", sub);
+var directhost = createVirtualHost("localhost", direct);
+app.use(subhost);
+app.use(directhost);
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use('/api', api);
+//app.use('/api', api);
 
 app.listen(env.NODE_PORT || config.port, env.NODE_IP || '0.0.0.0', function () {
     console.log('Application worker ' + process.pid + ' started...'); // eslint-disable-line
